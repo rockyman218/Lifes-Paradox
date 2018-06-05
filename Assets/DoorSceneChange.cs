94fba052f2af86ee93586a784dd6099632facbcd
Jakob Leech <rockyman218@gmail.com>
2018-06-05 19:42:08 +1200

    Can't remember

---
+++ Assets/DoorSceneChange.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DoorSceneChange : MonoBehaviour {
+
+	public Image fadingBackground;
+	public int nextScene;
+	public Animator fadingAnimator;
+
+	/// <summary>
+	/// Sent when another object enters a trigger collider attached to this
+	/// object (2D physics only).
+	/// </summary>
+	/// <param name="other">The other Collider2D involved in this collision.</param>
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		Debug.Log("Testing");
+		if(other.tag == "Player"){
+			fade();
+		}
+	}
+
+	void fade (){
+		fadingAnimator.SetTrigger("isFadingToLighGray");
+	}
+}
